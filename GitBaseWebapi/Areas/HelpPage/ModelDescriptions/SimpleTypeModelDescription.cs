eeaf625b617e1a0ea40c130e2af24b4564f81bbb
User <User@MDEVPC-23>
2018-01-17 14:19:53 +0530

    Add project files.

---
+++ GitBaseWebapi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GitBaseWebapi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ GitBaseWebapi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitBaseWebapi.WebApiApplication" Language="C#" %>
